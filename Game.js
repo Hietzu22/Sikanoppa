52d33be13f6aa025a96cfbc7138a4cdd58bc44fc
Hietzu <veeti.hietanen03@gmail.com>
2020-04-02 17:41:54 +0300

    more designing on the game, added player amount picker and name picker, added score counter and fixed a few bugs

--- Game.js
+++ Game.js
@@ -1,3 +1,18 @@
+var score = 0;
+var gamers = document.getElementById("gamers")
+
+let x = prompt('How many players are playing');
+
+let players = [];
+
+for (let i=0; i<x; i++){
+    players.push(prompt("Give player "+ (i*1+1) + "'s nimi"));
+}
+
+for (let i=0; i<players.length; i++){
+    document.write('Player ' + players + ': ' + players[i] + '<br>');
+}
+
 function rollDice() {
     var die1 = document.getElementById("die1");
     var die2 = document.getElementById("die2");
@@ -12,17 +27,34 @@ function rollDice() {
 
     if (d1 == 1 && d2 == 1){
         status.innerHTML = "Snake eyes! You get "+snakeEyes+" points!";
-    } else if(d1 == d2){
+        score = score + 25;
+    } else if (d1 == d2){
         status.innerHTML = "You rolled "+diceTotal+".";
         status.innerHTML += " DOUBLES! You get "+doubles+" points!";
-    } else if (d1 || d2) {
-        status.innerHTML = "You rolled "+diceTotal+"."
-    } else if (d1 || d2 == 1){
-        status.innerHTML = "Turn over! You got 0 points."
+        score = score + doubles;
+    } else if (d1 == 1 || d2 == 1) {
+        status = turnOver();
+        score = 0;
+    } else {
+        status.innerHTML = "You rolled "+diceTotal+".";
+        score = score + diceTotal;
     }
+    gameScore()
+}
+
+function gameScore() {
+    var gameScore = document.getElementById("gameScore");
+    gameScore.innerHTML = "Score: " + score;
+
 }
 
 function endTurn() {
     var status = document.getElementById("status");
-    status.innerHTML = "You ended your turn. You got points!";
+    status.innerHTML = "You ended your turn. You got " + score + " points!";
+    score = 0;
+}
+
+function turnOver() {
+    var status = document.getElementById("status");
+    status.innerHTML = "Turn over! You got 0 points this round.";
 }

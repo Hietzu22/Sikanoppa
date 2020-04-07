8517a4e263dc7de2714f8a21a54e69283380b1f4
Hietzu <veeti.hietanen03@gmail.com>
2020-04-07 13:01:09 +0300

    added point goal

--- Game.js
+++ Game.js
@@ -1,19 +1,15 @@
 var score = 0;
 var gamers = document.getElementById("gamers")
 
-let x = prompt('How many players are playing');
+let x = prompt('How many players are playing?');
 
 let players = [];
 
 for (let i=0; i<x; i++){
-    players.push(prompt("Give player "+ (i*1+1) + "'s nimi"));
+    players.push(prompt("Give player "+ (i*1+1) + "'s name"));
 }
 
-for (let i=0; i<players.length; i++){
-    document.write('Player ' + (i*1+1) + ': ' + players[i] + '<br>');
-}
-
-players[i] = '<span style="color:red">' + players[i] + '</span>';
+let goal = prompt('How many points to win the game?');
 
 function rollDice() {
     var die1 = document.getElementById("die1");
@@ -27,22 +23,29 @@ function rollDice() {
     var snakeEyes = 25;
     var doubles = diceTotal * 2;
 
-    if (d1 == 1 && d2 == 1){
+    if (d1 == 1 && d2 == 1) {
         status.innerHTML = "Snake eyes! You get "+snakeEyes+" points!";
         score = score + 25;
-    } else if (d1 == d2){
+    } else if (d1 == d2) {
         status.innerHTML = "You rolled "+diceTotal+".";
         status.innerHTML += " DOUBLES! You get "+doubles+" points!";
         score = score + doubles;
     } else if (d1 == 1 || d2 == 1) {
         status = turnOver();
         score = 0;
-    } else {
+    } else  {
         status.innerHTML = "You rolled "+diceTotal+".";
         score = score + diceTotal;
+    } if (goal <= score) {
+        status = gameWon();
     }
     gameScore()
 }
+
+for (let i=0; i<players.length; i++){
+    document.write('Player ' + (i*1+1) + ': ' + players[i] + ' Points: ' + score + '<br>');
+}
+
 
 function gameScore() {
     var gameScore = document.getElementById("gameScore");
@@ -59,4 +62,9 @@ function endTurn() {
 function turnOver() {
     var status = document.getElementById("status");
     status.innerHTML = "Turn over! You got 0 points this round.";
+}
+
+function gameWon() {
+    var status = document.getElementById("status");
+    status.innerHTML = "You Win!";
 }
